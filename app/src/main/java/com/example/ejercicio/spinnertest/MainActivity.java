06d751733a4ad157ae3b2a66d2d1a897e916f9a2
dspaez <dspaez@uce.edu.ec>
2016-11-23 22:33:02 -0500

    Initial commit

--- app/src/main/java/com/example/ejercicio/spinnertest/MainActivity.java
+++ app/src/main/java/com/example/ejercicio/spinnertest/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.ejercicio.spinnertest;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,6 +15,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Si Funciona Sincronizar GITHUB
 
         Spinner sp = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(
